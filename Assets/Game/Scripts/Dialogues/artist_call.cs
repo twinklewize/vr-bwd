610b2d7c503495272e3333a5c76f002d4df45b92
twinklewize <ivan.stanovoy@yahoo.com>
2023-01-24 16:06:53 +0300

    feat: new lightning and dialogues draft

---
+++ Assets/Game/Scripts/Dialogues/artist_call.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class artist_call : MonoBehaviour
+{
+    public GameObject playerText;
+
+    private string text = "Это Кристина Крейл. В доме что-то странное. Я наняла вас расследовать пропажу красок, но боюсь дело сложнее. Я не сумасшедшая, но все картины, вся мебель, стены, все стало черно-белым. Я так понимаю вы уже прибыли на место и сами все видите. Мне страшно, сегодня я переночую в мастерской музея";
+    // Start is called before the first frame update
+   void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/talking_with_deliver.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class talking_with_deliver : MonoBehaviour
+{
+    public Canvas text_label;
+    private string txt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  txt = text_label.text;
+    }
+
+    //private void OnTriggerStay(Collider other){
+    //    //Debug.Log("sheeeeet");
+    //    if(other.CompareTag("Player")){
+    //        text_label.enabled = true;
+    //        if(Input.GetKey(KeyCode.E) || OVRInput.Get(OVRInput.Button.One)){
+    //            //text_label.enabled = false;
+    //            StartCoroutine(waiter());
+    //           // text_label.text = txt;
+    //        }
+    //    }
+        
+    //}txt
+
+    //private void OnTriggerExit(Collider other){
+    //    text_label.text = txt;
+    //    text_label.enabled = false;
+    //}
+
+    //IEnumerator waiter(){
+    //    text_label.text = "Deliver: Hello detective!";
+
+    //    yield return new WaitForSeconds(3);
+
+    //    text_label.text = "Detective: Hello! What are you doing here now?";
+
+    //    yield return new WaitForSeconds(3);
+
+    //    text_label.text = "Deliver: I brought pizza, and there police stoped me and";
+
+    //    yield return new WaitForSeconds(3);
+
+    //    text_label.text = "Deliver: accuses of kidnapping!";
+
+    //    yield return new WaitForSeconds(3);
+
+    //    text_label.text = "Detective: I'm working on this case";
+
+    //    yield return new WaitForSeconds(3);
+    //}
+}
